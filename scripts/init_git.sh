367af19ef07afd2fe1479b438f5ab3def4662b92
Matheus de Souza <mws.souza@gmail.com>
2020-02-28 13:34:47 -0300

    [ADD] git configuration script

---
+++ scripts/init_git.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git config --global user.name "Matheus de Souza"
+git config --global user.email "mws.souza@gmail.com"
+
+git config --global core.editor "vim"
+
+echo 'Git configuration done!'
