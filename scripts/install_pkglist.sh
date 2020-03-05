3f01d5858c2870682b35eef96af931488b2fd783
Matheus de Souza <mws.souza@gmail.com>
2020-03-05 20:09:08 -0300

    [UPD] Improved package install/update scripts

--- scripts/install_pkglist.sh
+++ scripts/install_pkglist.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+command_exists () {
+    type "$1" &> /dev/null ;
+}
+
+AUR_PROGRAM="yay"
+DEFAULT_DIR="${HOME}/scripts"
+
+echo 'Tell me what installation is (Ex. arch, arch_vm):'; read INSTALL_TYPE;
 
 sudo pacman -S --needed - < pkglist.txt
+
+yay -S --needed - < pkglist.txt
+
+sudo pacman -S --needed - < "${DEFAULT_DIR}/pkglist_${INSTALL_TYPE}.txt"
+
+if  command_exists $AUR_PROGRAM ; then
+    yay -S --needed - < "${DEFAULT_DIR}/pkglist_${INSTALL_TYPE}_${AUR_PROGRAM}.txt"
+fi
