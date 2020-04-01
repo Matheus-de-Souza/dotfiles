9b9684d6bc7d7aebbe46b28f8dff59b2a739b2f5
Matheus de Souza <mws.souza@gmail.com>
2020-04-01 14:46:34 -0300

    [ADD] Script for install Chrome/Brave extensions

---
+++ scripts/install_browser_extensions.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+xdg-open https://chrome.google.com/webstore/detail/pro-for-trello-free-trell/hcjkfaengbcfeckhjgjdldmhjpoglecc # Pro For Trello
+xdg-open https://chrome.google.com/webstore/detail/lastpass-free-password-ma/hdokiejnpimakedhajhdlcegeplioahd # Last Pass
+xdg-open https://chrome.google.com/webstore/detail/cvim/ihlenndgcmojhcghmfjfneahoeklbjjh # cVim
+
+
